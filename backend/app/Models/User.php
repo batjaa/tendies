49678056e33542ac408658458c2644d6dea13d14
Batjaa Batbold <batjaa0615@gmail.com>
2026-03-04 17:50:46 -0800

    fix: cast trial_ends_at to datetime on User model

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -28,6 +28,7 @@ class User extends Authenticatable
     {
         return [
             'email_verified_at' => 'datetime',
+            'trial_ends_at' => 'datetime',
             'password' => 'hashed',
         ];
     }
